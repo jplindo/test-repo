3b2b5767d11327aca3145cd655a6de0a07f55670
jplindo <janus.lindo@gmail.com>
2018-01-18 13:51:31 -0600

    Add files via upload

--- InitWebServer.ps1
+++ InitWebServer.ps1
@@ -1,4 +1,4 @@
-# Grab a copy of our index file
-$IndexPage = "https://github.com/jplindo/test-repo/blob/master/index.asp";
+# Grab a copy of our index file and put it in the default web root directory
+$IndexPage = "https://raw.githubusercontent.com/jplindo/test-repo/master/index.asp";
 $OutputFile = "C:\\inetpub\wwwroot\\index.asp";
-Invoke-WebRequest -Uri $IndexPage -OutFile $output
+Invoke-WebRequest -Uri $IndexPage -OutFile $OutputFile
