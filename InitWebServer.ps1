74369961082858e4de436d3ec94896d46147fe29
jplindo <janus.lindo@gmail.com>
2018-01-18 14:24:30 -0600

    Add files via upload

--- InitWebServer.ps1
+++ InitWebServer.ps1
@@ -1,3 +1,7 @@
+# Enable Web Server and install features to run our default index file
+Install-WindowsFeature -name Web-Server -IncludeManagementTools;
+Enable-WindowsOptionalFeature -Online -FeatureName "IIS-WebServerRole", "IIS-ApplicationDevelopment", "IIS-ISAPIFilter", "IIS-ISAPIExtensions", "IIS-ASP";
+
 # Grab a copy of our index file and put it in the default web root directory
 $IndexPage = "https://raw.githubusercontent.com/jplindo/test-repo/master/index.asp";
 $OutputFile = "C:\\inetpub\wwwroot\\index.asp";
