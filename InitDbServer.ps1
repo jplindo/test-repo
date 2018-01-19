d28f8c05d3ffd0ce5c6c405b7cb2e76b351c4abd
jplindo <janus.lindo@gmail.com>
2018-01-18 20:25:52 -0600

    Add files via upload

--- InitDbServer.ps1
+++ InitDbServer.ps1
@@ -1,6 +1,6 @@
 # Download db script and execute to create TestDB database on this machine
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/jplindo/test-repo/master/TestDB.sql -OutFile .\TestDB.sql
-Invoke-Sqlcmd -InputFile .\TestDB.sql -ConnectionString "Data Source=localhost;Integrated Security=True;"
+Invoke-Sqlcmd -InputFile .\TestDB.sql -HostName "localhost"
 
 # Open Firewall Port
 New-NetFirewallRule -DisplayName "MSSQLSERVER Inbound TCP Port" -Direction Inbound -LocalPort 1433 -Protocol TCP -Action Allow
